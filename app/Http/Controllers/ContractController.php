706858a7796950baaf514d98446fb2a2da8c9a6c
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2015-07-04 08:33:22 +0000

    worked on rules for entering new contracts
    - Date Checks
    - Can’t add a propery that is already rented

--- app/Http/Controllers/ContractController.php
+++ app/Http/Controllers/ContractController.php
@@ -5,6 +5,9 @@ use App\Http\Requests\Request;
 use App\Http\Requests\contractRequest;
 use Carbon\Carbon;
 use App\Contract;
+use App\Property;
+use App\Tenant;
+
 
 class ContractController extends Controller 
 {
@@ -37,16 +40,58 @@ class ContractController extends Controller
    */
   public function store(contractRequest $request)
   {
+    
     $effective = new Carbon($request->effective_date);
+    // set effective day to first day of month;
+    $effective->day = 1;
+
     $expiry = new Carbon($request->expiry_date);
+    // set day of expiry to last day of month
+    $expiry->day = $expiry->daysInMonth;
+
+    // check if effective date is > expiry date
     if ( $effective > $expiry) {
       \Session::flash('message', 'Expiry date cannot be set before effective date');
       return redirect()->route('contracts.create')->withInput();
     }
-    dd('all ok');
-    // THINGS TO DO BEFORE STORING:
-    // - Verify that expiry date is after effective date
-    // - Verify that properties are not bound to other contracts in the time frame
+
+    // check if properties selected are not committed (need to test this)
+    $properties = $request->properties;
+    $committedProperties = "";
+
+    foreach ($properties as $key => $property) {
+      $propertyInFocus = Property::findorFail($property);
+
+      if ($effective < $propertyInFocus->latestContractExpiryDate()  ) {
+        $committedProperties .= " $propertyInFocus->description";
+      }
+    }
+
+    if (strlen($committedProperties) > 0 ) {
+      \Session::flash('message', 'The following properties are already committed in that date range: ' . $committedProperties);
+      return redirect()->route('contracts.create')->withInput();
+    }
+
+    // if all is ok
+    $contract = new Contract;
+    $contract->description = $request->description;
+    $contract->effective_date = $effective;
+    $contract->expiry_date = $expiry;
+
+    // associate with tenant
+    $tenant = Tenant::findOrFail($request->tenant);
+    $contract->tenant()->associate($tenant);
+
+    $contract->save();
+
+    // attach properties
+    foreach ($properties as $key => $property) {
+      $propertyInFocus = Property::findOrFail($property);
+      $contract->properties()->attach($propertyInFocus);
+    }
+
+    return redirect()->route('contracts.index');
+
   }
 
   /**
